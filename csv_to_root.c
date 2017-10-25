c680d5ab426dfc2c87d6b257fdb5261e165e15af
Matthias Dubouchet <beulard@gmail.com>
2017-10-25 17:53:07 +0100

    one csv file for all spectra, compiled to root format with csv_to_root.c and the full spectrum is shown by nuspectrum.c. next step is propagating the oscillations

--- csv_to_root.c
+++ csv_to_root.c
@@ -1,26 +1,9 @@
 
 void csv_to_root() {
-	TNtuple* mu_data = new TNtuple("spectrum", "", "f");
-	TNtuple* antimu_data = new TNtuple("spectrum", "", "f");
-	TNtuple* e_data = new TNtuple("spectrum", "", "f");
-	TNtuple* antie_data = new TNtuple("spectrum", "", "f");
+	TTree* spectrum = new TTree("spectrum", "");
+	spectrum->ReadFile("data/spectrum.csv", "mu/F:antimu:e:antie", ',');
 	
-	mu_data->ReadFile("data/mu.csv");
-	antimu_data->ReadFile("data/antimu.csv");
-	e_data->ReadFile("data/e.csv");
-	antie_data->ReadFile("data/antie.csv");
-	
-	TFile* f = new TFile("data/mu.root", "RECREATE");
-	mu_data->Write();
-	f->Close();
-	f = new TFile("data/antimu.root", "RECREATE");
-	antimu_data->Write();
-	f->Close();
-	f = new TFile("data/e.root", "RECREATE");
-	e_data->Write();
-	f->Close();
-	f = new TFile("data/antie.root", "RECREATE");
-	antie_data->Write();
-
+	TFile* f = new TFile("data/spectrum.root", "RECREATE");
+	spectrum->Write();
 	f->Close();
 }
--- nuspectrum.c
+++ nuspectrum.c
@@ -1,34 +1,56 @@
 
 
 void nuspectrum() {
-	TFile* f = TFile::Open("data/mu.root");
-	if(f->IsZombie()) {
-		Printf("caca");
-	}
-	TNtuple* mutree = (TNtuple*)f->Get("spectrum");	
+	TFile* f = TFile::Open("data/spectrum.root");
 
 	TH1* h1 = new TH1F("h1", "", 50, 0., 10.);
+	TH1* h2 = new TH1F("h2", "", 50, 0., 10.);
+	TH1* h3 = new TH1F("h3", "", 50, 0., 10.);
+	TH1* h4 = new TH1F("h4", "", 50, 0., 10.);
 	
+	float mu_vals[50];
+	float antimu_vals[50];
+	float e_vals[50];
+	float antie_vals[50];
+
+	TTree* t = (TTree*)f->Get("spectrum");
+	t->Print();
 
 	TTreeReader r("spectrum", f);
-	TTreeReaderValue<float> mu(r, "f");
+	TTreeReaderValue<float> mu(r, "mu");
+	TTreeReaderValue<float> antimu(r, "antimu");
+	TTreeReaderValue<float> e(r, "e");
+	TTreeReaderValue<float> antie(r, "antie");
+
 	int i = 0;
 	while(r.Next()) {
 		h1->Fill(i * 0.2, *mu);
+		h2->Fill(i * 0.2, *antimu);
+		h3->Fill(i * 0.2, *e);
+		h4->Fill(i * 0.2, *antie);
+
+		mu_vals[i] = *mu;
+		antimu_vals[i] = *antimu;
+		e_vals[i] = *e;
+		antie_vals[i] = *antie;	
+
 		++i;
 	}
 
 	gStyle->SetOptStat(0);
 	
+	//	initial spectrum plots
 	TCanvas* c1 = new TCanvas("c1", "", 700, 600);
 	c1->SetLogy();	
 	h1->SetLineColor(1);
 	h1->SetMinimum(1e6);
 	h1->SetMaximum(1e10);
 	h1->Draw("HIST");
-	
-	//	TODO maybe arrange data in one root file if possible
-	//	draw other curves
-	//	start propagating neutrinos
+	h2->SetLineColor(4);	
+	h2->Draw("SAME HIST");
+	h3->SetLineColor(2);
+	h3->Draw("SAME HIST");
+	h4->SetLineColor(6);
+	h4->Draw("SAME HIST");
 	
 }
